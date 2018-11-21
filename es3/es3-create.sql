a8bfdc1d77c50ad03cd8dbb08a06a97aeec3fe5f
marty-p <37085112+marty-p@users.noreply.github.com>
2018-11-21 09:29:09 +0100

    +es3 +simulazione_negozio +conn

---
+++ es3/es3-create.sql
@@ -0,0 +1,26 @@
+﻿create schema prodotti;
+
+CREATE TABLE prodotti.magazzino
+(
+	codM char(3) primary key,
+	indirizzo varchar(50) not null,
+	citta varchar(50) not null
+);
+
+CREATE TABLE prodotti.prodotto
+(
+	codP char(3) primary key,
+	nome varchar(50) not null,
+	categoria varchar(50) not null
+);
+
+CREATE TABLE prodotti.inventario
+(
+	magazzino char(3) references prodotti.magazzino
+		on delete cascade on update cascade,
+	prodotto char(3) references prodotti.prodotto
+		on delete cascade on update cascade,
+	quantita integer /*smallint*/ not null CHECK (quantita > 0),
+	prezzo float /*decimal(7,2)*/ not null CHECK (prezzo > 0.00),
+	primary key (magazzino, prodotto)
+);
---
+++ simulazione_negozio/create.sql
@@ -0,0 +1,34 @@
+﻿create schema negozio;
+
+/*replace serial with smallint*/
+
+--drop table negozio.vetrina;
+create table negozio.vetrina(
+codiceV serial primary key,
+nomeVetrina varchar(50) unique not null,
+nomeReparto varchar(50) check(nomeReparto IN ('uomo','donna','bambino')) not null,
+--nomeReparto varchar(50) check(nomeReparto 0 ANY ('{"uomo","donna","bambino"}')) not null,
+metratura int check(metratura between 10 and 200)
+);
+
+--drop table negozio.prodotto;
+create table negozio.prodotto(
+codiceP serial primary key,
+nomeProdotto varchar(50) not null,
+marca varchar(50) not null,
+categoria varchar(50) not null,
+prezzo float not null check (prezzo > 1)
+);
+
+--drop table negozio.presenza;
+create table negozio.presenza(
+vetrina serial references negozio.vetrina
+	on delete cascade
+	on update cascade,
+prodotto serial references negozio.prodotto
+	on delete no action
+	on update cascade,
+quantita int check (quantita > 0),
+primary key(vetrina, prodotto)
+);
+
