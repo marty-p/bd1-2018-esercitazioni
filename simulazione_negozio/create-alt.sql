3de5dfb41f2315313cae421e880c0399ff3d9f20
marty-p <37085112+marty-p@users.noreply.github.com>
2018-11-22 08:17:49 +0100

    Update create-alt.sql

--- simulazione_negozio/create-alt.sql
+++ simulazione_negozio/create-alt.sql
@@ -1,6 +1,5 @@
 ﻿create schema negozio;
 
---drop table negozio.vetrina;
 create table negozio.vetrina(
 codiceV smallint primary key,
 nomeVetrina varchar(50) unique not null,
@@ -9,7 +8,6 @@ nomeReparto varchar(50) check(nomeReparto IN ('uomo','donna','bambino')) not nul
 metratura int check(metratura between 10 and 200)
 );
 
---drop table negozio.prodotto;
 create table negozio.prodotto(
 codiceP smallint primary key,
 nomeProdotto varchar(50) not null,
@@ -18,7 +16,6 @@ categoria varchar(50) not null,
 prezzo decimal(7, 2) not null check (prezzo > 1)
 );
 
---drop table negozio.presenza;
 create table negozio.presenza(
 vetrina smallint references negozio.vetrina
 	on delete cascade
