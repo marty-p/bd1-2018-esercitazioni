fe68221efa49294a52a8133896e512bf7dcad825
marty-p <37085112+marty-p@users.noreply.github.com>
2018-11-23 14:02:55 +0000

    Update create.sql

--- esercitazione-04/create.sql
+++ esercitazione-04/create.sql
@@ -1,20 +1,20 @@
-create schema public;
+create schema progetti;
 
-create table public.progetto(
+create table progetti.progetto(
 	codp char(3) primary key,
 	nome varchar(50),
 	anno int,
 	budget int check (budget > 10000) not null
 );
 
-create table public.dipendente(
+create table progetti.dipendente(
 	codp char(3) primary key,
 	cognome varchar(50) not null,
 	nome varchar(50) not null,
 	citta varchar(50)
 );
 
-create table public.partecipa(
+create table progetti.partecipa(
 	progetto char(3) references public.progetto,
 	dipendente char(3) references public.dipendente,
 	mesi int check (mesi between 3 and 24),
