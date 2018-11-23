40dd3faae9d494f43d1f98c6c2b500dffe4cac34
marty-p <37085112+marty-p@users.noreply.github.com>
2018-11-23 14:12:36 +0000

    Update create.sql

--- esercitazione-04/create.sql
+++ esercitazione-04/create.sql
@@ -3,7 +3,7 @@ create schema progetti;
 create table progetti.progetto(
 	codp char(3) primary key,
 	nome varchar(50),
-	anno int,
+	anno /*small*/int,
 	budget int check (budget > 10000) not null
 );
 
@@ -21,6 +21,7 @@ create table progetti.partecipa(
 	dipendente char(3) references progetti.dipendente
 		on update no action
 		on delete cascade,
-	mesi int check (mesi between 3 and 24),
-	ruolo varchar(50)
+	mesi /*small*/int check (mesi between 3 and 24),
+	ruolo varchar(50),
+	primary key(progetto, dipendente)
 );
