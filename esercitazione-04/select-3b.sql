75f3f0a27b301bde4655faef6693ecc7b36cc53b
marty-p <37085112+marty-p@users.noreply.github.com>
2018-11-30 15:48:33 +0100

    Update select-3b.sql

--- esercitazione-04/select-3b.sql
+++ esercitazione-04/select-3b.sql
@@ -4,40 +4,60 @@
 h) Selezionare il codice di quei dipendenti che partecipano ai progetti con almeno tre ruoli
 distinti.*/
 
-/* USANDO DISTINCT */
+/* USANDO DISTINCT
 select dipendente
 from progetti.partecipa
 group by dipendente
-having count(distinct ruolo) >= 3;
+having count(distinct ruolo) >= 3;*/
 
 
 /*____________________________________________________
 i) Selezionare, per ogni dipendente e per ogni ruolo che ricopre, il numero di progetti in cui
 ricopre quel ruolo.*/
 
-/* VERIFICA VELOCE CON ORDER BY */
+/* VERIFICA VELOCE CON ORDER BY
 select *
 from progetti.partecipa
-order by dipendente, ruolo;
+order by dipendente, ruolo;*/
 
-/* USANDO GROUP BY */
+/* USANDO GROUP BY
 select dipendente, ruolo, count(ruolo)
 from progetti.partecipa
-group by dipendente, ruolo;
+group by dipendente, ruolo;*/
 
 
 /*____________________________________________________
 j) Selezionare, per ogni anno, il budget complessivo destinato ai progetti di quell'anno.*/
+
+/*select anno, sum(budget)
+from progetti.progetto
+group by anno;*/
 
 
 /*____________________________________________________
 k) Selezionare, per ogni progetto a cui partecipano dipendenti per un numero complessivo di
 mesi superiore a 15, il numero distinto di ruoli che partecipano a quel progetto.*/
 
+/* VERIFICA VELOCE
+select *
+from progetti.partecipa
+order by progetto, mesi*/
+
+/* USANDO DISTINCT
+select progetto, count(distinct ruolo)
+from progetti.partecipa
+group by progetto
+having sum(mesi) > 15;*/
+
 
 /*____________________________________________________
 l) Selezionare, per ogni progetto, il numero complessivo di mesi destinati ad ogni ruolo (tra
 quelli che partecipano al progetto).*/
+
+select progetto, ruolo, sum(mesi)
+from progetti.partecipa
+group by progetto, ruolo
+order by progetto, ruolo;
 
 
 /*____________________________________________________
