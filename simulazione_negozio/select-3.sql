4fb3383ef152e896b7e7a893b4b111bf62bb3e47
marty-p <37085112+marty-p@users.noreply.github.com>
2018-11-30 16:24:48 +0100

    Add files via upload

---
+++ simulazione_negozio/select-3.sql
@@ -0,0 +1,26 @@
+﻿/*3) Interrogazioni SQL e viste
+Scrivere il codice SQL per effettuare le seguenti operazioni:*/
+
+/*___________________________________________________________
+a) Selezionare nome, marca e categoria dei prodotti presenti in vetrine del reparto 'donna' il cui
+prezzo sia inferiore a 130, ordinati per marca ed eliminando i duplicati.*/
+
+
+/*___________________________________________________________
+b) Selezionare la metratura della vetrina più piccola in cui è presente almeno un prodotto di
+marca 'Trussardi'.*/
+
+
+/*___________________________________________________________
+c) Selezionare i dati delle vetrine che vendono esclusivamente prodotti il cui costo sia
+superiore al costo medio dei prodotti*/
+
+
+/*___________________________________________________________
+d) Selezionare i dati dei prodotti che sono presenti in almeno 2 vetrine, esclusi i prodotti
+presenti nelle vetrine il cui reparto è ‘bambino’*/
+
+
+/*___________________________________________________________
+e) Selezionare il codice delle coppie di prodotti che sono venduti dalla stessa vetrina.*/
+
