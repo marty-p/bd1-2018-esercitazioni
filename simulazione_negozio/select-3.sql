7c1d3199cb441ff961075893fc39e769552f403b
marty-p <37085112+marty-p@users.noreply.github.com>
2018-11-30 16:34:46 +0100

    Update select-3.sql

--- simulazione_negozio/select-3.sql
+++ simulazione_negozio/select-3.sql
@@ -1,10 +1,21 @@
-﻿/*3) Interrogazioni SQL e viste
+/*3) Interrogazioni SQL e viste
 Scrivere il codice SQL per effettuare le seguenti operazioni:*/
 
 /*___________________________________________________________
 a) Selezionare nome, marca e categoria dei prodotti presenti in vetrine del reparto 'donna' il cui
 prezzo sia inferiore a 130, ordinati per marca ed eliminando i duplicati.*/
 
+select distinct nomeProdotto, marca, categoria
+from negozio.prodotto
+where prezzo < 130
+and codicep in (
+	select prodotto
+	from negozio.presenza
+	join negozio.vetrina on vetrina = codicev
+	join negozio.prodotto on prodotto = codicep
+	where nomeReparto = 'donna'
+)
+order by marca;
 
 /*___________________________________________________________
 b) Selezionare la metratura della vetrina più piccola in cui è presente almeno un prodotto di
