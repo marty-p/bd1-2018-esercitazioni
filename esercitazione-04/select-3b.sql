6bf652bb1b3635243cd808c8325853bab618389b
marty-p <37085112+marty-p@users.noreply.github.com>
2018-11-30 15:14:08 +0100

    Add files via upload

---
+++ esercitazione-04/select-3b.sql
@@ -0,0 +1,33 @@
+﻿/*3b) Interrogazioni – seconda parte: raggruppamento*/
+
+/*____________________________________________________
+h) Selezionare il codice di quei dipendenti che partecipano ai progetti con almeno tre ruoli
+distinti.*/
+
+
+/*____________________________________________________
+i) Selezionare, per ogni dipendente e per ogni ruolo che ricopre, il numero di progetti in cui
+ricopre quel ruolo.*/
+
+
+/*____________________________________________________
+j) Selezionare, per ogni anno, il budget complessivo destinato ai progetti di quell'anno.*/
+
+
+/*____________________________________________________
+k) Selezionare, per ogni progetto a cui partecipano dipendenti per un numero complessivo di
+mesi superiore a 15, il numero distinto di ruoli che partecipano a quel progetto.*/
+
+
+/*____________________________________________________
+l) Selezionare, per ogni progetto, il numero complessivo di mesi destinati ad ogni ruolo (tra
+quelli che partecipano al progetto).*/
+
+
+/*____________________________________________________
+m) Selezionare il progetto a cui sono destinati complessivamente più mesi.*/
+
+
+/*____________________________________________________
+n) Selezionare i dati dei progetti cui partecipano almeno tre dipendenti che abitano a Cagliari o
+a Sassari.*/
