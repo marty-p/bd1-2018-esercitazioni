b9c33d408135041b18891f63a9500bddaddd9594
marty-p <37085112+marty-p@users.noreply.github.com>
2018-12-07 15:15:02 +0100

    Update create.sql

--- esercitazione-05/create.sql
+++ esercitazione-05/create.sql
@@ -5,12 +5,12 @@ create table prestiti.cliente (
 	cognome varchar(50) not null,
 	nome varchar(50) not null,
 	citta_residenza varchar(50),
-	unique(cognome, nome)
+	unique(nome, cognome)
 );
 
 create table prestiti.filiale (
 	idfiliale char(3) primary key,
-	importo_max decimal(9, 2) not null,
+	importo_max decimal(12, 2) not null,
 	citta varchar(50)
 );
 
@@ -19,7 +19,7 @@ create table prestiti.prestito (
 	filiale char(3) references prestiti.filiale
 		on delete no action
 		on update cascade,
-	importo decimal(9, 2) not null,
+	importo decimal(12, 2) not null,
 	data_accensione date not null check (data_accensione < data_scadenza),
 	data_scadenza date not null
 
