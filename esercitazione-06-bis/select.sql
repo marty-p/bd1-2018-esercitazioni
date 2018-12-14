f580bcbf078cf01a38d625eec257b39c459f6e68
marty-p <37085112+marty-p@users.noreply.github.com>
2018-12-14 16:48:33 +0100

    Update select.sql

--- esercitazione-06-bis/select.sql
+++ esercitazione-06-bis/select.sql
@@ -30,16 +30,32 @@ group by codi;
 /*___________________________________________
 f) Selezionare i dati degli agenti che hanno effettuato almeno due visite ma nessuna vendita.*/
 
+/* TUTOR */
 select * from immobili.agente
 where coda in (
 	select coda from immobili.statistiche
 	where num_visite >= 2 and num_vendite = 0
 );
 
+/* MIA */
+select agente.* from immobili.statistiche
+natural join immobili.agente
+where num_visite >= 2 and num_vendite = 0;
+
 
 /*___________________________________________
 g) Per ciascun agente che ha effettuato almeno due visite e almeno due vendite, determinare la
 data dell’ultima visita effettuata e la data dell’ultima vendita effettuata.*/
+
+select VI.coda, max(VE.data), max(VI.data)
+from immobili.vendita VE
+join immobili.visita VI on VI.coda = VE.coda
+where VE.coda in (
+	select coda from immobili.statistiche
+	where num_visite >= 2 and num_vendite >= 2
+)
+group by VI.coda;
+
 
 /*___________________________________________
 h) Determinare, fra tutti gli immobili invenduti, quali sono quelli più cari per la zona e il tipo a
