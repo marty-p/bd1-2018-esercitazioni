77c0c0b5f4bc9d3c4c29b6c8a789e6edb8e87e51
marty-p <37085112+marty-p@users.noreply.github.com>
2018-12-14 15:12:26 +0100

    Update create.sql

--- esercitazione-06/create.sql
+++ esercitazione-06/create.sql
@@ -7,3 +7,17 @@ create table immobili.immobile(
 	zona varchar(50) not null check (zona in ('residenziale', 'centro', 'periferia')),
 	prezzo_richiesto decimal(9, 2) not null
 );
+
+create table immobili.agente(
+	coda char(3) primary key,
+	cognome varchar(50),
+	nome varchar(50),
+	agenzia varchar(50) not null
+);
+
+create table immobili.visita(
+	codi char(3) references immobili.immobile,
+	coda char(3) references immobili.agente,
+	unique (codi, coda)
+);
+
