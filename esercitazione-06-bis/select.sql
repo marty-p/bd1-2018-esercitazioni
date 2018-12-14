1ca0ed888f1f52f7d1dd481b7c483f3ed5e2e446
marty-p <37085112+marty-p@users.noreply.github.com>
2018-12-14 16:25:36 +0100

    Create select.sql

---
+++ esercitazione-06-bis/select.sql
@@ -0,0 +1,18 @@
+/*4) Uso delle viste nelle interrogazioni
+ Facendo uso delle viste precedentemente definite, formulare le seguenti interrogazioni:*/
+
+/*___________________________________________
+e) Per ciascun immobile ancora invenduto, determinare il numero di volte in cui è stato
+visitato.*/
+
+/*___________________________________________
+f) Selezionare i dati degli agenti che hanno effettuato almeno due visite ma nessuna vendita.*/
+
+/*___________________________________________
+g) Per ciascun agente che ha effettuato almeno due visite e almeno due vendite, determinare la
+data dell’ultima visita effettuata e la data dell’ultima vendita effettuata.*/
+
+/*___________________________________________
+h) Determinare, fra tutti gli immobili invenduti, quali sono quelli più cari per la zona e il tipo a
+cui si riferiscono (ovvero il più caro degli appartamenti invenduti in zona residenziale, il più
+caro degli appartamenti invenduti in centro, etc...). */
