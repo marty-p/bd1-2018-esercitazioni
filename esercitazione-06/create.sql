832e8878b6cbd9db72b55c97940b5fe4d064cc80
marty-p <37085112+marty-p@users.noreply.github.com>
2018-12-14 15:31:37 +0100

    Update create.sql

--- esercitazione-06/create.sql
+++ esercitazione-06/create.sql
@@ -2,30 +2,38 @@ create schema immobili;
 
 create table immobili.immobile(
 	codi char(3) primary key,
-	indirizzo varchar(50) not null,
-	tipo varchar(50) not null check (tipo in ('appartamento', 'villetta', 'magazzino')),
-	zona varchar(50) not null check (zona in ('residenziale', 'centro', 'periferia')),
-	prezzo_richiesto decimal(9, 2) not null
+	indirizzo varchar(64) not null,
+	tipo varchar(64) not null check (tipo in ('appartamento', 'villetta', 'magazzino')),
+	zona varchar(64) not null check (zona in ('residenziale', 'centro', 'periferia')),
+	prezzo_richiesto decimal(15, 2) not null
 );
 
 create table immobili.agente(
 	coda char(3) primary key,
-	cognome varchar(50),
-	nome varchar(50),
-	agenzia varchar(50) not null
+	cognome varchar(64),
+	nome varchar(64),
+	agenzia varchar(64) not null
 );
 
 create table immobili.visita(
-	codi char(3) references immobili.immobile,
-	coda char(3) references immobili.agente,
+	codi char(3) references immobili.immobile
+		on delete cascade
+		on update cascade,
+	coda char(3) references immobili.agente
+		on delete no action
+		on update cascade,
 	data date,
-	unique(codi, coda)
+	primary key(codi, coda, data)
 );
 
 create table immobili.vendita(
-	codi char(3) references immobili.immobile,
-	coda char(3) references immobili.agente not null,
+	codi char(3) references immobili.immobile
+		on delete no action
+		on update cascade,
+	coda char(3) references immobili.agente
+		on delete no action
+		on update cascade,
 	data date,
-	prezzo decimal(9, 2) not null, -- check (select * from immobili.immobile where prezzo >= prezzo_richiesto)
-	unique(codi, coda)
+	prezzo decimal(9, 2) not null,
+	primary key(codi)
 );
