67598fee13e6021c575ab082ab102c0befada26f
marty-p <37085112+marty-p@users.noreply.github.com>
2018-12-21 15:03:14 +0100

    Add files via upload

---
+++ simulazione_esame_palestra/create.sql
@@ -0,0 +1,24 @@
+﻿create schema palestra;
+
+create table palestra.corso(
+	codicec serial primary key,
+	nomecorso varchar(126) not null,
+	nomeistruttore varchar(126) not null,
+	capienza int check (capienza between 15 and 30)
+);
+
+create table palestra.atleta(
+	codicea serial primary key,
+	nomeatleta varchar(126) not null,
+	cognomeatleta varchar(126) not null,
+	eta int,
+	categoria varchar(126) not null check (categoria in ('principiante', 'intermedio', 'esperto'))
+);
+
+create table palestra.iscrizione(
+	corso serial references palestra.corso,
+	atleta serial references palestra.atleta,
+	abbonamento varchar(126) check ( abbonamento in ('open', 'singolo')),
+	primary key (corso, atleta)
+);
+
